daa2ebf798f372e127aefa461296f65547a23631
Timm Ruland <timm.heine.ruland@iais.fraunhofer.de>
2024-01-25 13:33:14 +0100

    feat: switched to lower case for the graph and queries to it

--- src/fact_finder/__main__.py
+++ src/fact_finder/__main__.py
@@ -22,8 +22,14 @@ assert OPENAI_API_KEY is not None
 async def on_chat_start():
     graph = Neo4jGraph(url=NEO4J_URL, username=NEO4J_USER, password=NEO4J_PW)
     model = ChatOpenAI(model="gpt-4", streaming=False, temperature=0, api_key=OPENAI_API_KEY)  # gpt-3.5-turbo-16k
-    neo4j_chain = GraphCypherQAChain.from_llm(model, graph=graph, cypher_prompt=CYPHER_GENERATION_PROMPT,
-                                              qa_prompt=CYPHER_QA_PROMPT, verbose=True, return_intermediate_steps=True)
+    neo4j_chain = GraphCypherQAChain.from_llm(
+        model,
+        graph=graph,
+        cypher_prompt=CYPHER_GENERATION_PROMPT,
+        qa_prompt=CYPHER_QA_PROMPT,
+        verbose=True,
+        return_intermediate_steps=True,
+    )
     llm_chain = LLMChain(llm=model, prompt=LLM_PROMPT, verbose=True)
     cl.user_session.set("neo4j_chain", neo4j_chain)
     cl.user_session.set("llm_chain", llm_chain)
@@ -34,7 +40,10 @@ async def main(message: cl.Message):
     llm_chain = cl.user_session.get("llm_chain")
     llm_chain_res = await llm_chain.ainvoke(input=message.content, callbacks=[cl.LangchainCallbackHandler()])
     neo4j_chain = cl.user_session.get("neo4j_chain")
-    neo4j_chain_res = await neo4j_chain.ainvoke(input=message.content, callbacks=[cl.LangchainCallbackHandler()])
+    neo4j_chain_res = await neo4j_chain.ainvoke(
+        input=message.content.lower(), callbacks=[cl.LangchainCallbackHandler()]
+    )
     concatenated_answer = concatenate_with_headers(
-        [{"LLM:": llm_chain_res['text']}, {"Graph:": neo4j_chain_res["result"]}])
+        [{"LLM:": llm_chain_res["text"]}, {"Graph:": neo4j_chain_res["result"]}]
+    )
     await cl.Message(content=concatenated_answer).send()
