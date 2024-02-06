01bb94684b44ab520a61ca3ed755311035dc080a
Timm Ruland <timm.heine.ruland@iais.fraunhofer.de>
2024-02-06 11:57:09 +0100

    refactor(tools): refactored EntityDetector

--- src/fact_finder/entity_detector/entity_detector.py
+++ src/fact_finder/entity_detector/entity_detector.py
@@ -1,5 +1,6 @@
 import json
 import os
+from typing import List
 
 import requests
 from dotenv import load_dotenv
@@ -7,37 +8,28 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+_DEFAULT_ENTITY_FILENAMES = [
+    "sci_gene_human_dictionary.txt",  # Human genes
+    "sci_ab.dictionary.txt",  # Antibodies
+    "sci_disease_dictionary.txt",  # Diseases
+    "hubble_cells.dictionary.txt",  # Cells
+    "sci_drug_dictionary.txt",  # Drugs
+    "organ_thesarus.txt",  # Organs
+    "celline.dictionary.txt",  # Celllines
+]
+
+
 class EntityDetector:
 
-    def __init__(self, filenames: list[str] = None):
+    def __init__(self, filenames: List[str] = _DEFAULT_ENTITY_FILENAMES):
+        self.__possible_filenames = filenames
         self.__url = "https://api2.linguist.skgt.int.bayer.com/linnaeusannotate"
         self.__api_key = os.getenv("SYNONYM_API_KEY")
-        if filenames:
-            self.__possible_filenames = filenames
-        else:
-            self.__possible_filenames = self.__default_filenames()
-
-    def __default_filenames(self) -> list[str]:
-        return [
-            "sci_gene_human_dictionary.txt",  # Human genes
-            "sci_ab.dictionary.txt",  # Antibodies
-            "sci_disease_dictionary.txt",  # Diseases
-            "hubble_cells.dictionary.txt",  # Cells
-            "sci_drug_dictionary.txt",  # Drugs
-            "organ_thesarus.txt",  # Organs
-            "celline.dictionary.txt",  # Celllines
-        ]
+        assert self.__api_key is not None, "For using EntityDetector, the env variable SYNONYM_API_KEY must be set."
 
     def __call__(self, search_text: str) -> dict:
         filenames_as_single_string = ", ".join(self.__possible_filenames)
-        payload = json.dumps({
-            "public_dic": filenames_as_single_string,
-            "text": search_text
-        })
-        headers = {
-            'x-api-key': self.__api_key,
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
+        payload = json.dumps({"public_dic": filenames_as_single_string, "text": search_text})
+        headers = {"x-api-key": self.__api_key, "Accept": "application/json", "Content-Type": "application/json"}
         response = requests.request("POST", self.__url, headers=headers, data=payload)
         return json.loads(response.text)["annotations"]
