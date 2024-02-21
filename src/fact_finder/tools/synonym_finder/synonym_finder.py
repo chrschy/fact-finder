80162043dbdfde861cdcf1a5d8cf0c5229d14eb6
Timm Ruland <timm.heine.ruland@iais.fraunhofer.de>
2024-02-21 12:42:28 +0100

    refactor(tools): Moved synonym finder into subdirectory of tools.
    Also, minor refactoring.

--- src/fact_finder/tools/synonym_finder/synonym_finder.py
+++ src/fact_finder/tools/synonym_finder/synonym_finder.py
@@ -9,21 +9,25 @@ from SPARQLWrapper import JSON, SPARQLWrapper
 class SynonymFinder(ABC):
 
     @abstractmethod
-    def __call__(self, node: str) -> List[str]:
+    def __call__(self, name: str) -> List[str]:
         pass
 
 
 class WikiDataSynonymFinder(SynonymFinder):
 
-    def __init__(self, endpoint_url: str = "https://query.wikidata.org/sparql", user_agent: str = "factfinder/1.0"):
+    def __init__(
+        self,
+        endpoint_url: str = "https://query.wikidata.org/sparql",
+        user_agent: str = "factfinder/1.0",
+    ):
         self.__endpoint_url = endpoint_url
         self.__user_agent = user_agent
 
-    def __call__(self, node: str) -> List[str]:
-        query = self.__generate_sparql_forwards_query(node)
+    def __call__(self, name: str) -> List[str]:
+        query = self.__generate_sparql_forwards_query(name)
         results = self.__get_sparql_results(query)
         if not results:
-            query = self.__generate_sparql_backwards_query(node)
+            query = self.__generate_sparql_backwards_query(name)
             results = self.__get_sparql_results(query)
         return results
 
@@ -36,40 +40,44 @@ class WikiDataSynonymFinder(SynonymFinder):
         results = [result["label"]["value"] for result in results["results"]["bindings"]]
         return results
 
-    def __generate_sparql_forwards_query(self, node: str) -> str:
+    def __generate_sparql_forwards_query(self, name: str) -> str:
         query = """SELECT DISTINCT ?label WHERE {
                   ?s skos:altLabel "%s"@en.
                   ?s rdfs:label ?label .
                   FILTER(str(lang(?label)) = "en")
-                }""" % (node)
+                }""" % (
+            name
+        )
         return query
 
-    def __generate_sparql_backwards_query(self, node: str) -> str:
+    def __generate_sparql_backwards_query(self, name: str) -> str:
         # todo test and adjust the query such that it works when node="alcohol" -> "ethanol"
         query = """SELECT DISTINCT ?alt_label WHERE {
                 ?s skos:altLabel ?alt_label .
                 ?s rdfs:label "%s"@en .
                 FILTER(str(lang(?label)) = "en")
-                }""" % (node)
+                }""" % (
+            name
+        )
         return query
 
 
 class WordNetSynonymFinder(SynonymFinder):
 
-    def __call__(self, node: str) -> List[str]:
-        result = wn.synonyms(word=node)[0]
+    def __call__(self, name: str) -> List[str]:
+        result = wn.synonyms(word=name)[0]
         return result
 
 
 class SimilaritySynonymFinder(SynonymFinder):
 
-    def __call__(self, node: str) -> List[str]:
+    def __call__(self, name: str) -> List[str]:
         # todo find most similar node from all nodes with vector cosine similarity search
         raise NotImplementedError
 
 
 class SubWordSynonymFinder(SynonymFinder):
 
-    def __call__(self, node: str) -> List[str]:
+    def __call__(self, name: str) -> List[str]:
         # todo 2010.11784.pdf (arxiv.org)
         raise NotImplementedError
