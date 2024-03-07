7592238c79eab017c235f564ff3b0aa65477a4de
Timm Ruland <timm.heine.ruland@iais.fraunhofer.de>
2024-03-07 11:06:49 +0100

    test(chains): Updated and refactored tests for AnswerGenerationChain.

--- tests/chains/test_answer_generation_chain.py
+++ tests/chains/test_answer_generation_chain.py
@@ -1,36 +1,72 @@
-from typing import Any, Dict, List, Optional
-from unittest.mock import MagicMock
+from typing import Any, Dict, List
+from unittest.mock import ANY, MagicMock
 
 import pytest
-from langchain.chains import LLMChain
-from langchain_core.callbacks import CallbackManagerForChainRun
-from langchain_core.language_models import BaseChatModel
-from langchain_core.messages import AIMessage
-from langchain_core.outputs import ChatGeneration, LLMResult
-from langchain_core.prompts import PromptTemplate
-
 from fact_finder.chains.answer_generation_chain import AnswerGenerationChain
-from fact_finder.prompt_templates import CYPHER_QA_PROMPT
+from langchain_core.language_models import BaseChatModel, BaseLanguageModel
+from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts.string import StringPromptValue
+from tests.chains.helpers import build_llm_mock
 
 
-def test_qa_chain(
-    answer_generation_chain: AnswerGenerationChain, output_from_graph_chain: Dict[str, Any], expected_answer
+def test_output_key_in_result(
+    answer_generation_chain: AnswerGenerationChain,
+    output_from_graph_chain: Dict[str, Any],
 ):
     result = answer_generation_chain.invoke(input=output_from_graph_chain)
     assert answer_generation_chain.output_key in result.keys()
-    assert result[answer_generation_chain.output_key] == expected_answer
+
+
+def test_returned_result_matches_model_output(
+    answer_generation_chain: AnswerGenerationChain, output_from_graph_chain: Dict[str, Any], llm_answer: str
+):
+    result = answer_generation_chain.invoke(input=output_from_graph_chain)
+    assert result[answer_generation_chain.output_key] == llm_answer
+
+
+def test_answer_generation_llm_is_called_with_expected_arguments(
+    answer_generation_chain: AnswerGenerationChain,
+    llm: BaseLanguageModel,
+    generation_prompt: str,
+    output_from_graph_chain: Dict[str, Any],
+):
+    answer_generation_chain.invoke(input=output_from_graph_chain)
+    llm.generate_prompt.assert_called_once_with([generation_prompt], None, callbacks=ANY)
 
 
 @pytest.fixture
-def answer_generation_chain(llm: BaseChatModel, llm_chain: LLMChain) -> AnswerGenerationChain:
-    answer_generation_chain = AnswerGenerationChain(llm=llm, prompt_template=CYPHER_QA_PROMPT)
-    answer_generation_chain.llm_chain = llm_chain
+def generation_prompt(
+    prompt_template: PromptTemplate, graph_result: List[Dict[str, str]], question: str
+) -> StringPromptValue:
+    return prompt_template.format_prompt(context=graph_result, question=question)
+
+
+@pytest.fixture
+def answer_generation_chain(llm: BaseChatModel, prompt_template: PromptTemplate) -> AnswerGenerationChain:
+    answer_generation_chain = AnswerGenerationChain(llm=llm, prompt_template=prompt_template)
     return answer_generation_chain
 
 
 @pytest.fixture
-def llm() -> BaseChatModel:
-    return MagicMock(spec=BaseChatModel)
+def prompt_template() -> PromptTemplate:
+    return PromptTemplate(
+        input_variables=["context", "question"], template="Generate an answer to {question} using {context}:"
+    )
+
+
+@pytest.fixture
+def llm(llm_answer) -> BaseLanguageModel:
+    return build_llm_mock(llm_answer)
+
+
+@pytest.fixture
+def llm_answer() -> str:
+    return (
+        "The drugs associated with epilepsy are phenytoin, valproic acid, "
+        "lamotrigine, diazepam, clonazepam, fosphenytoin, mephenytoin, "
+        "neocitrullamon, carbamazepine, phenobarbital, secobarbital, "
+        "primidone, and lorazepam."
+    )
 
 
 @pytest.fixture
@@ -39,14 +75,9 @@ def prompt() -> PromptTemplate:
 
 
 @pytest.fixture
-def llm_chain(llm: BaseChatModel, prompt: PromptTemplate) -> LLMChain:
-    return MockedLLMChain(llm=llm, prompt=prompt)
-
-
-@pytest.fixture
-def output_from_graph_chain() -> Dict[str, Any]:
+def output_from_graph_chain(question: str, graph_result: List[Dict[str, str]]) -> Dict[str, Any]:
     return {
-        "question": "Which drugs are associated with epilepsy?",
+        "question": question,
         "cypher_query": "MATCH (d:drug)-[:indication]->(dis:disease) WHERE dis.name = 'epilepsy' RETURN d.name",
         "intermediate_steps": [
             {"question": "Which drugs are associated with epilepsy?"},
@@ -54,78 +85,39 @@ def output_from_graph_chain() -> Dict[str, Any]:
                 "FormatPreprocessor": 'MATCH (d:drug)-[:indication]->(dis:disease)\nWHERE dis.name = "epilepsy"\nRETURN d.name',
                 "LowerCasePropertiesCypherQueryPreprocessor": 'MATCH (d:drug)-[:indication]->(dis:disease)\nWHERE dis.name = "epilepsy"\nRETURN d.name',
             },
-            {
-                "graph_result": [
-                    {"d.name": "phenytoin"},
-                    {"d.name": "phenytoin"},
-                    {"d.name": "phenytoin"},
-                    {"d.name": "valproic acid"},
-                    {"d.name": "lamotrigine"},
-                    {"d.name": "lamotrigine"},
-                    {"d.name": "diazepam"},
-                    {"d.name": "clonazepam"},
-                    {"d.name": "fosphenytoin"},
-                    {"d.name": "mephenytoin"},
-                    {"d.name": "mephenytoin"},
-                    {"d.name": "neocitrullamon"},
-                    {"d.name": "carbamazepine"},
-                    {"d.name": "carbamazepine"},
-                    {"d.name": "phenobarbital"},
-                    {"d.name": "phenobarbital"},
-                    {"d.name": "secobarbital"},
-                    {"d.name": "primidone"},
-                    {"d.name": "primidone"},
-                    {"d.name": "lorazepam"},
-                ]
-            },
+            {"graph_result": graph_result},
         ],
         "preprocessed_cypher_query": 'MATCH (d:drug)-[:indication]->(dis:disease)\nWHERE dis.name = "epilepsy"\nRETURN d.name',
-        "graph_result": [
-            {"d.name": "phenytoin"},
-            {"d.name": "phenytoin"},
-            {"d.name": "phenytoin"},
-            {"d.name": "valproic acid"},
-            {"d.name": "lamotrigine"},
-            {"d.name": "lamotrigine"},
-            {"d.name": "diazepam"},
-            {"d.name": "clonazepam"},
-            {"d.name": "fosphenytoin"},
-            {"d.name": "mephenytoin"},
-            {"d.name": "mephenytoin"},
-            {"d.name": "neocitrullamon"},
-            {"d.name": "carbamazepine"},
-            {"d.name": "carbamazepine"},
-            {"d.name": "phenobarbital"},
-            {"d.name": "phenobarbital"},
-            {"d.name": "secobarbital"},
-            {"d.name": "primidone"},
-            {"d.name": "primidone"},
-            {"d.name": "lorazepam"},
-        ],
+        "graph_result": graph_result,
     }
 
 
 @pytest.fixture
-def expected_answer():
-    return "The drugs associated with epilepsy are phenytoin, valproic acid, lamotrigine, diazepam, clonazepam, fosphenytoin, mephenytoin, neocitrullamon, carbamazepine, phenobarbital, secobarbital, primidone, and lorazepam."
+def question() -> str:
+    return "Which drugs are associated with epilepsy?"
 
 
-class MockedLLMChain(LLMChain):
-    def generate(
-        self,
-        input_list: List[Dict[str, Any]],
-        run_manager: Optional[CallbackManagerForChainRun] = None,
-    ) -> LLMResult:
-        return LLMResult(
-            generations=[
-                [
-                    ChatGeneration(
-                        text="The drugs associated with epilepsy are phenytoin, valproic acid, lamotrigine, diazepam, clonazepam, fosphenytoin, mephenytoin, neocitrullamon, carbamazepine, phenobarbital, secobarbital, primidone, and lorazepam.",
-                        generation_info={"finish_reason": "stop", "logprobs": None},
-                        message=AIMessage(
-                            content="The drugs associated with epilepsy are phenytoin, valproic acid, lamotrigine, diazepam, clonazepam, fosphenytoin, mephenytoin, neocitrullamon, carbamazepine, phenobarbital, secobarbital, primidone, and lorazepam."
-                        ),
-                    )
-                ]
-            ],
-        )
+@pytest.fixture
+def graph_result() -> List[Dict[str, str]]:
+    return [
+        {"d.name": "phenytoin"},
+        {"d.name": "phenytoin"},
+        {"d.name": "phenytoin"},
+        {"d.name": "valproic acid"},
+        {"d.name": "lamotrigine"},
+        {"d.name": "lamotrigine"},
+        {"d.name": "diazepam"},
+        {"d.name": "clonazepam"},
+        {"d.name": "fosphenytoin"},
+        {"d.name": "mephenytoin"},
+        {"d.name": "mephenytoin"},
+        {"d.name": "neocitrullamon"},
+        {"d.name": "carbamazepine"},
+        {"d.name": "carbamazepine"},
+        {"d.name": "phenobarbital"},
+        {"d.name": "phenobarbital"},
+        {"d.name": "secobarbital"},
+        {"d.name": "primidone"},
+        {"d.name": "primidone"},
+        {"d.name": "lorazepam"},
+    ]
