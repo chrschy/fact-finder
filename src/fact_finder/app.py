d4a5f2df0ec275fa7cca2e860db1aae1e2c4a425
Daniel Steinigen <develop@steinigen.me>
2024-03-15 15:21:05 +0100

    fix(ui): add more error handling

--- src/fact_finder/app.py
+++ src/fact_finder/app.py
@@ -135,19 +135,35 @@ def get_html(html: str, legend=False):
 
 
 async def call_neo4j(message):
-    return st.session_state.neo4j_chain.invoke(message)  # FIXME use ainvoke?
+    try:
+        return st.session_state.neo4j_chain.invoke(message)  # FIXME use ainvoke?
+    except Exception as e:
+        print(e)
+        return {"graph_qa_output": {}}
 
 
 async def call_llm(message):
-    return st.session_state.llm_chain.invoke(message)  # FIXME use ainvoke?
+    try:
+        return st.session_state.llm_chain.invoke(message)  # FIXME use ainvoke?
+    except Exception as e:
+        print(e)
+        return {"text": ""}
 
 
 async def call_rag(message):
-    return st.session_state.rag_chain.invoke(message)  # FIXME use ainvoke?
-  
-  
+    try:
+        return st.session_state.rag_chain.invoke(message)  # FIXME use ainvoke?
+    except Exception as e:
+        print(e)
+        return {"rag_output": ""}
+
+
 async def call_summary(sub_graph: str):
-    return st.session_state.summary_chain.invoke(sub_graph)  # FIXME use ainvoke?
+    try:
+        return st.session_state.summary_chain.invoke(sub_graph)  # FIXME use ainvoke?
+    except Exception as e:
+        print(e)
+        return {"summary": ""}
 
 
 async def call_chains(message):
@@ -256,69 +272,6 @@ def _process_nodes_only(entry, graph_converted: Subgraph, result_ents: list) -> 
             except Exception as e:
                 print(e)
     return graph_triplet
-
-
-def _contains_triple(entry):
-    return len([value for key, value in entry.items() if type(value) is tuple]) > 0
-
-
-def _process_triple(entry, graph_converted: Subgraph, result_ents: list, idx_rel: int) -> None:
-    trip = [value for key, value in entry.items() if type(value) is tuple][0]
-    head_type = [key for key, value in entry.items() if value == trip[0]]
-    tail_type = [key for key, value in entry.items() if value == trip[2]]
-    head_type = head_type[0] if len(head_type) > 0 else ""
-    tail_type = tail_type[0] if len(tail_type) > 0 else ""
-    node_head = trip[0] if "index" in trip[0] else list(entry.values())[0]
-    node_tail = trip[2] if "index" in trip[2] else list(entry.values())[2]
-
-    if "index" in node_head and node_head["index"] not in [node.id for node in graph_converted.nodes]:
-        graph_converted.nodes.append(
-            Node(
-                id=node_head["index"],
-                type=head_type,
-                name=node_head["name"],
-                in_query=False,
-                in_answer=node_head["name"] in result_ents,
-            )
-        )
-    if "index" in node_tail and node_tail["index"] not in [node.id for node in graph_converted.nodes]:
-        graph_converted.nodes.append(
-            Node(
-                id=node_tail["index"],
-                type=tail_type,
-                name=node_tail["name"],
-                in_query=False,
-                in_answer=node_tail["name"] in result_ents,
-            )
-        )
-    if "index" in node_head and "index" in node_tail:
-        graph_converted.edges.append(
-            Edge(
-                id=idx_rel,
-                type=trip[1],
-                name=trip[1],
-                source=node_head["index"],
-                target=node_tail["index"],
-                in_query=False,
-                in_answer=node_tail["name"] in result_ents,
-            )
-        )
-
-
-def _process_nodes_only(entry, graph_converted: Subgraph, result_ents: list) -> None:
-    for variable_binding, possible_node in entry.items():
-        if not isinstance(possible_node, dict):
-            continue
-        if "index" in possible_node and possible_node["index"] not in [node.id for node in graph_converted.nodes]:
-            graph_converted.nodes.append(
-                Node(
-                    id=possible_node["index"],
-                    type=variable_binding,
-                    name=possible_node["name"],
-                    in_query=False,
-                    in_answer=possible_node["name"] in result_ents,
-                )
-            )
 
 
 def request_pipeline(text_data: str):
