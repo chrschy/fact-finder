cb1f6db7bf8473667c4546bb76207204601ebd11
mrudat <max.rudat@iais.fraunhofer.de>
2024-04-09 12:54:03 +0200

    refactor(score): model is now loaded in __init__()

--- src/fact_finder/evaluator/score/embedding_score.py
+++ src/fact_finder/evaluator/score/embedding_score.py
@@ -10,9 +10,11 @@ class EmbeddingScore(Score):
     2. compare the vectors (cosine similarity)
     """
 
+    def __init__(self):
+        self._model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
     def compare(self, text_a: str, text_b: str) -> float:
-        model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-        embedding_1 = model.encode(text_a, convert_to_tensor=True)
-        embedding_2 = model.encode(text_b, convert_to_tensor=True)
+        embedding_1 = self._model.encode(text_a, convert_to_tensor=True)
+        embedding_2 = self._model.encode(text_b, convert_to_tensor=True)
         similarity_tensor = util.pytorch_cos_sim(embedding_1, embedding_2)[0]
         return float(similarity_tensor)
