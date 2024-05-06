cce9b82b8e495688e1d472754a30bcdb7e951dec
Timm Ruland <timm.heine.ruland@iais.fraunhofer.de>
2024-05-06 11:57:01 +0200

    feat(evaluation): Set default value for less used fields of EvaluationSample.

--- src/fact_finder/evaluator/evaluation_sample.py
+++ src/fact_finder/evaluator/evaluation_sample.py
@@ -1,12 +1,13 @@
+from typing import Any, Dict, List
+
 from pydantic import BaseModel
-from typing import List, Dict, Any
 
 
 class EvaluationSample(BaseModel):
     question: str
     cypher_query: str
     question_is_answerable: bool
-    source: str
-    expected_answer: str
+    source: str = ""
+    expected_answer: str = ""
     sub_graph: List[Dict] = []
     nodes: List[Dict[str, Any]] = []
