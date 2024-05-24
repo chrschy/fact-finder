eeb22e79a2d4343989f300f46979d56515c9f1a6
Roman Teucher <Roman.Teucher@iais.fraunhofer.de>
2024-05-24 09:48:38 +0200

    feat(evaluator): WIP add adversarial attack eval

--- src/fact_finder/evaluator/adversarial_attack_evaluation.py
+++ src/fact_finder/evaluator/adversarial_attack_evaluation.py
@@ -1,9 +1,11 @@
 from functools import partial
-from typing import Dict, Any
+from typing import Dict, Any, List, Optional
 
+from langchain.chains.base import Chain
 from langchain_core.callbacks import CallbackManagerForChainRun
 from langchain_core.language_models import BaseLanguageModel
 from langchain_core.prompts import BasePromptTemplate
+from pydantic.v1 import Extra
 
 from fact_finder.chains import GraphQAChain, CypherQueryGenerationChain
 from fact_finder.chains.filtered_primekg_question_preprocessing_chain import FilteredPrimeKGQuestionPreprocessingChain
@@ -15,44 +17,58 @@ from fact_finder.config.primekg_config import (
     _get_primekg_entity_categories,
 )
 from fact_finder.config.primekg_predicate_descriptions import PREDICATE_DESCRIPTIONS
+from fact_finder.evaluator.evaluation_sample import EvaluationSample
+from fact_finder.evaluator.evaluation_samples import manual_samples
 from fact_finder.prompt_templates import KEYWORD_PROMPT, COMBINED_QA_PROMPT
 from fact_finder.tools.entity_detector import EntityDetector
 from fact_finder.utils import load_chat_model, build_neo4j_graph
 
 
-QUESTION_WRONG_CYPHER_MAPPING = {}
+QUESTION_WRONG_CYPHER_MAPPING = {
+    "Which medications have more off-label uses than approved indications?": 'MATCH (d:drug  {name:"lamotrigine"})-[:side_effect]-(e) RETURN e'
+}
 
 
-class AdversarialCypherQueryGenerationChain(CypherQueryGenerationChain):
+class AdversarialCypherQueryGenerationChain(Chain):
     """
     Class only used for evaluation of adversarial attacks. It simply returns a wrong cypher query given a natural
     language question. Should downstream test the verbalization of it.
     """
+    input_key: str = "question"  #: :meta private:
+    output_key: str = "cypher_query"  #: :meta private:
+    @property
+    def input_keys(self) -> List[str]:
+        return ["question"]
 
-    def __init__(
-        self,
-        llm: BaseLanguageModel,
-        prompt_template: BasePromptTemplate,
-        graph_structured_schema: Dict[str, Any],
-        adversarial_question_query_mapping: Dict[str, str],
-    ):
-        super().__init__(llm, prompt_template, graph_structured_schema)
-        self.__adversarial_question_query_mapping = adversarial_question_query_mapping
+    @property
+    def output_keys(self) -> List[str]:
+        return ["cypher_query"]
 
-        ...
+    def _call(self, inputs: Dict[str, Any], run_manager: Optional[CallbackManagerForChainRun] = None) -> Dict[str, Any]:
+        query = self._generate_cypher(inputs)
+        return self._prepare_chain_result(inputs=inputs, generated_cypher=query)
 
-    def _generate_cypher(self, inputs: Dict[str, Any], run_manager: CallbackManagerForChainRun) -> str:
-        return self.__adversarial_question_query_mapping[inputs[self.input_key]]
+    def _prepare_chain_result(self, inputs: Dict[str, Any], generated_cypher: str) -> Dict[str, Any]:
+        chain_result = {self.output_key: generated_cypher}
+
+        intermediate_steps = inputs.get("intermediate_steps", [])
+
+        intermediate_steps += [
+            {"question": inputs[self.input_key]},
+            {self.output_key: generated_cypher},
+            {f"{self.__class__.__name__}_filled_prompt": "n/a"},
+        ]
+        chain_result["intermediate_steps"] = intermediate_steps
+        return chain_result
+
+    def _generate_cypher(self, inputs: Dict[str, Any]) -> str:
+        return QUESTION_WRONG_CYPHER_MAPPING[inputs["question"]]
 
 
 class AdversarialAttackGraphQAChain(GraphQAChain):
 
-    def __init__(self, config: GraphQAChainConfig, adversarial_cypher_chain: AdversarialCypherQueryGenerationChain):
-        super().__init__(config)
-        self.__adversarial_cypher_chain = adversarial_cypher_chain
-
-    def _build_cypher_query_generation_chain(self, config: GraphQAChainConfig) -> CypherQueryGenerationChain:
-        return self.__adversarial_cypher_chain
+    def _build_cypher_query_generation_chain(self, config: GraphQAChainConfig) -> AdversarialCypherQueryGenerationChain:
+        return AdversarialCypherQueryGenerationChain()
 
 
 class AdversarialAttackEvaluation:
@@ -63,13 +79,13 @@ class AdversarialAttackEvaluation:
     def evaluate(self): ...
 
 
-def build_chain(model: BaseLanguageModel, combine_output_with_sematic_scholar: bool, args: List[str] = []) -> Chain:
+def build_chain(args: List[str] = []) -> Chain:
     parsed_args = _parse_primekg_args(args)
     graph = build_neo4j_graph()
     cypher_preprocessors = _build_preprocessors(graph, parsed_args.normalized_graph)
     cypher_prompt, answer_generation_prompt = _get_graph_prompt_templates()
     config = GraphQAChainConfig(
-        llm=model,
+        llm=load_chat_model(),
         graph=graph,
         cypher_prompt=cypher_prompt,
         answer_generation_prompt=answer_generation_prompt,
@@ -81,20 +97,33 @@ def build_chain(model: BaseLanguageModel, combine_output_with_sematic_scholar: b
         entity_detector=EntityDetector() if parsed_args.use_entity_detection_preprocessing else None,
         allowed_types_and_description_templates=_get_primekg_entity_categories(),
         use_subgraph_expansion=parsed_args.use_subgraph_expansion,
-        combine_output_with_sematic_scholar=combine_output_with_sematic_scholar,
+        combine_output_with_sematic_scholar=False,
         semantic_scholar_keyword_prompt=KEYWORD_PROMPT,
         combined_answer_generation_prompt=COMBINED_QA_PROMPT,
     )
-    return AdversarialAttackGraphQAChain(
-        config,
-        AdversarialCypherQueryGenerationChain(
-            llm=load_chat_model(),
-            prompt_template=config.cypher_prompt,
-            graph_structured_schema=config.graph.get_structured_schema,
-            adversarial_question_query_mapping=QUESTION_WRONG_CYPHER_MAPPING,
-        ),
-    )
+
+    return AdversarialAttackGraphQAChain(config=config)
+
+
+def eval_samples(limit_of_samples: int = None):
+    eval_samples = []
+    for manual_sample in manual_samples[:limit_of_samples]:
+        eval_sample = EvaluationSample(
+            question=manual_sample["question"],
+            cypher_query=manual_sample["expected_cypher"],
+            expected_answer=manual_sample["expected_answer"],
+            nodes=manual_sample["nodes"],
+        )
+        eval_samples.append(eval_sample)
+    return eval_samples
 
 
 if __name__ == "__main__":
-    ...
+    chain = build_chain(args=["--normalized_graph", "--use_entity_detection_preprocessing"])
+    # chain = build_chain(args=["--normalized_graph"])
+    samples = eval_samples(limit_of_samples=1)
+    for sample in samples:
+        print(sample.question)
+        inputs = {"question": sample.question}
+        result = chain.invoke(inputs)
+        ...
