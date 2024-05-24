9eaafab30d668a6427b9beda8780509d1e54d9e8
Timm Ruland <timm.heine.ruland@iais.fraunhofer.de>
2024-05-24 12:48:31 +0200

    feat(preprocessor): Changed preferred term synonym finder to return the original name first in order to retain it if it is present in the graph.

--- src/fact_finder/tools/synonym_finder/synonym_finder.py
+++ src/fact_finder/tools/synonym_finder/synonym_finder.py
@@ -1,23 +1,23 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import Iterable, List
 
 
 class SynonymFinder(ABC):
 
     @abstractmethod
-    def __call__(self, name: str) -> List[str]:
+    def __call__(self, name: str) -> Iterable[str]:
         pass
 
 
 class SimilaritySynonymFinder(SynonymFinder):
 
-    def __call__(self, name: str) -> List[str]:
+    def __call__(self, name: str) -> Iterable[str]:
         # TODO find most similar node from all nodes with vector cosine similarity search
         raise NotImplementedError
 
 
 class SubWordSynonymFinder(SynonymFinder):
 
-    def __call__(self, name: str) -> List[str]:
+    def __call__(self, name: str) -> Iterable[str]:
         # TODO 2010.11784.pdf (arxiv.org)
         raise NotImplementedError
